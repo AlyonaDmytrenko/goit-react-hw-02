fb5c69570a51f6c454430de2d55ad735baacf7a0
AlyonaDmytrenko <143014363+AlyonaDmytrenko@users.noreply.github.com>
2024-03-19 17:22:14 +0200

    the task has been corrected (useState)

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,34 @@
-import { useEffect, useMemo, useState } from 'react';
+// App.js
+import { useEffect, useState } from 'react';
 import Description from './components/Description/Description';
 import Feedback from './components/Feedback/Feedback';
 import Options from './components/Options/Options';
 import Notification from './components/Notification/Notification';
 
+const initialFeedback = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+  total: 0,
+  positive: 0,
+};
+
 function App() {
-  const initialFeedbackState = useMemo(
-    () => ({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-      total: 0,
-      positive: 0,
-    }),
-    []
-  );
-
-  const [optionsFeedback, setOptionsFeedback] = useState(initialFeedbackState);
-
-  useEffect(() => {
-    const storedFeedback = localStorage.getItem('feedback');
-    if (storedFeedback) {
-      setOptionsFeedback(JSON.parse(storedFeedback));
-    } else {
-      setOptionsFeedback(initialFeedbackState);
-    }
-  }, [initialFeedbackState]);
+  const [optionsFeedback, setoptionsFeedback] = useState(() => {
+    const stringifiedFeedback = localStorage.getItem('feedback');
+    const parsedFeedback = JSON.parse(stringifiedFeedback) ?? initialFeedback;
+    return parsedFeedback;
+  });
 
   useEffect(() => {
     localStorage.setItem('feedback', JSON.stringify(optionsFeedback));
   }, [optionsFeedback]);
 
   const updateFeedback = feedbackType => {
-    setOptionsFeedback(prevOptionsFeedback => ({
-      ...prevOptionsFeedback,
-      [feedbackType]: prevOptionsFeedback[feedbackType] + 1,
-      total: prevOptionsFeedback.total + 1,
-    }));
-  };
-
-  const resetFeedback = () => {
-    setOptionsFeedback(initialFeedbackState);
+    setoptionsFeedback({
+      ...optionsFeedback,
+      [feedbackType]: optionsFeedback[feedbackType] + 1,
+    });
   };
 
   const totalFeedback =
@@ -50,6 +38,14 @@ function App() {
         ((optionsFeedback.good + optionsFeedback.neutral) / totalFeedback) * 100
       )
     : 0;
+
+  const resetFeedback = () => {
+    setoptionsFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
 
   return (
     <>
