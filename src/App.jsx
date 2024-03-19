3b9428ae7509729b7f60e2cd8fd60fb759dcc06c
AlyonaDmytrenko <143014363+AlyonaDmytrenko@users.noreply.github.com>
2024-03-19 10:10:40 +0200

    the task has been corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import Description from './components/Description/Description';
 import Feedback from './components/Feedback/Feedback';
 import Options from './components/Options/Options';
 import Notification from './components/Notification/Notification';
 
 function App() {
-  const [optionsFeedback, setOptions] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0,
-    positive: 0,
-  });
+  const initialFeedbackState = useMemo(
+    () => ({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+      total: 0,
+      positive: 0,
+    }),
+    []
+  );
+
+  const [optionsFeedback, setOptionsFeedback] = useState(initialFeedbackState);
 
   useEffect(() => {
     const storedFeedback = localStorage.getItem('feedback');
     if (storedFeedback) {
-      setOptions(JSON.parse(storedFeedback));
+      setOptionsFeedback(JSON.parse(storedFeedback));
+    } else {
+      setOptionsFeedback(initialFeedbackState);
     }
-  }, []);
+  }, [initialFeedbackState]);
 
   useEffect(() => {
     localStorage.setItem('feedback', JSON.stringify(optionsFeedback));
   }, [optionsFeedback]);
 
   const updateFeedback = feedbackType => {
-    setOptions({
-      ...optionsFeedback,
-      [feedbackType]: optionsFeedback[feedbackType] + 1,
-    });
+    setOptionsFeedback(prevOptionsFeedback => ({
+      ...prevOptionsFeedback,
+      [feedbackType]: prevOptionsFeedback[feedbackType] + 1,
+      total: prevOptionsFeedback.total + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setOptionsFeedback(initialFeedbackState);
   };
 
   const totalFeedback =
@@ -38,14 +50,6 @@ function App() {
         ((optionsFeedback.good + optionsFeedback.neutral) / totalFeedback) * 100
       )
     : 0;
-
-  const resetFeedback = () => {
-    setOptions({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
 
   return (
     <>
